2b8f5b8a314db3fc86b198468c6c50728e110dec
Ioana Drimbarean <ioana.drimbarean@accesa.eu>
2022-11-14 16:07:17 +0200

    [ReactiveFormsDepth] Add comments.

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,52 +1,46 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {createPromoRangeValidator} from '../../validators/date-range.validator';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { createPromoRangeValidator } from '../../validators/date-range.validator';
 
 @Component({
   selector: 'create-course-step-2',
   templateUrl: 'create-course-step-2.component.html',
-  styleUrls: ['create-course-step-2.component.scss']
+  styleUrls: ['create-course-step-2.component.scss'],
 })
 export class CreateCourseStep2Component implements OnInit {
-
-    form = this.fb.group({
-        courseType: ['premium', Validators.required],
-        price: [null, [
-            Validators.required,
-            Validators.min(1),
-            Validators.max(9999),
-            Validators.pattern("[0-9]+")
-        ]],
-        thumbnail: [null],
-        promoStartAt: [null],
-        promoEndAt: [null]
-    }, {
-        validators: [createPromoRangeValidator()]
-    });
-
-    constructor(private fb: FormBuilder) {
-
+  form = this.fb.group(
+    {
+      courseType: ['premium', Validators.required],
+      price: [
+        null,
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(9999),
+          Validators.pattern('[0-9]+'),
+        ],
+      ],
+      thumbnail: [null],
+      promoStartAt: [null],
+      promoEndAt: [null],
+    },
+    {
+      validators: [createPromoRangeValidator()], // Multi field validator!Validation at the level of form
     }
+  );
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    //this.form.valueChanges => obsevable emits a new values whenever any of the value changes
+    this.form.valueChanges.subscribe((val) => {
+      const priceControl = this.form.controls['price'];
 
-        this.form.valueChanges
-            .subscribe(val => {
-
-                const priceControl = this.form.controls["price"];
-
-                if (val.courseType == 'free' && priceControl.enabled) {
-                    priceControl.disable({emitEvent: false});
-                }
-                else if (val.courseType == 'premium' && priceControl.disabled) {
-                    priceControl.enable({emitEvent:false});
-                }
-
-            });
-
-
-
+      if (val.courseType == 'free' && priceControl.enabled) {
+        priceControl.disable({ emitEvent: false });
+      } else if (val.courseType == 'premium' && priceControl.disabled) {
+        priceControl.enable({ emitEvent: false });
+      }
+    });
   }
-
 }
